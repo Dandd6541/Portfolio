4bc336ea5b5945cfbb2a59d45286e1f324d2cb8f
Dandd6541 <Mordkid4@gmail.com>
2022-09-29 17:58:11 -0700

    css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const yummyButton = document.getElementById('yummy');
 const navList = document.getElementById('nav-list');
-
+const $resume = $(
+  '<a id= "resumeLink" href="https://docs.google.com/document/d/16RkqOFF5wDfEYcwhF3x5qyaMs2EKjouI/edit"style="color:black"><button style= "padding: 10px">Download Resume</button></a>'
+)
+$("#section").append($resume)
 
 function toggleButton() {
   navList.classList.toggle('show')
